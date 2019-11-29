8cc6e2ba51a2d23cea934d139150c44e42afcc28
lpabst <lorenpabst@gmail.com>
2019-11-29 16:28:34 -0700

    added massive

--- index.js
+++ index.js
@@ -2,17 +2,29 @@ require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
+const massive = require('massive');
 
 var app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get("/api/postTest", function (req, res) {
-    res.status(200).send({ test: process.env.TEST });
-});
+
+massive({
+    host: process.env.HOST,
+    database: "ddog0gtsau2890",
+    port: 5432,
+    user: "bztqfmczxjivqg",
+    password: "cfe163e9e823a68a0c65c22c2448be47911eded8ea11463f1eba3e348ebb806a",
+    ssl: true
+})
+    .then(db => {
+        app.set('db', db);
+    })
 
 app.get('/validateToken/:token', (req, res) => {
-    res.status(200).send({ token: req.params.token })
+    const db = req.app.get('db');
+    const token = await db.tokens.insert({ token: req.params.token })
+    res.status(200).send({ token })
 })
 
 app.listen(process.env.PORT, () => console.log(`Example app running`))
